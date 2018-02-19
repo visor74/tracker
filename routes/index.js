93418e1d14ffcfe96fded9acf5a867104c962814
Alex Sladkoff <asladkoff@gmail.com>
2018-02-20 01:15:24 +0200

    added views.modal.add_parcel.jade

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 let express = require('express');
-// let passport = require('passport');
+//  let passport = require('passport');
 let router = express.Router();
 let helpers = require('../lib/helpers');
-let parcelController = require('../lib/parcel_controller');
+let parcelController = require('../lib/parcelController');
 
 /* GET home page. */
 router.get('/', helpers.ensureAuthenticated, function(req, res) {
     res.render('index', { title: 'Tracker', user : req.user});
 });
 
-router.get('/parcel/create', parcelController.getCreateParcel());
+router.get('/parcel/add', parcelController.getCreateParcel);
 
 module.exports = router;
